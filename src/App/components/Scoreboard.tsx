91c518dacf0d50c45641382f06c3ce84e2a6cd26
Luce Liu <thisisluce@gmail.com>
2025-03-02 21:33:12 -0500

    display username in scoreboard

--- src/App/components/Scoreboard.tsx
+++ src/App/components/Scoreboard.tsx
@@ -2,12 +2,14 @@ import { useGame } from "../contexts/GameContext";
 
 const Scoreboard: React.FC = () => {
   const { gameState } = useGame();
-  const { playerScore, computerScore } = gameState;
+  const { playerScore, computerScore, username } = gameState;
 
   return (
     <div className="flex justify-center w-full max-w-md mb-6">
       <div className="bg-white shadow-md rounded-lg p-4 flex-1 mx-2 text-center">
-        <h3 className="text-sm text-gray-600">You</h3>
+        <h3 className="text-sm text-gray-600">
+          {!!username ? `${username} (You)` : "You"}
+        </h3>
         <p className="text-3xl font-bold text-indigo-600">{playerScore}</p>
       </div>
       <div className="bg-white shadow-md rounded-lg p-4 flex-1 mx-2 text-center">
