a491eef801e888cdfbb6b1f9f48514c10858c989
Luce Liu <thisisluce@gmail.com>
2025-03-02 23:33:42 -0500

    update emojis

--- src/App/constants/choices.ts
+++ src/App/constants/choices.ts
@@ -1,17 +1,9 @@
 import { Choice } from "../types/gameplay";
 
 export const ChoiceEmoji: Record<Choice, string> = {
-  [Choice.Rock]: "👊",
-  [Choice.Paper]: "✋",
-  [Choice.Scissors]: "✌️",
+  [Choice.Rock]: "🪨",
+  [Choice.Paper]: "📄",
+  [Choice.Scissors]: "✂️",
   [Choice.Lizard]: "🦎",
   [Choice.Spock]: "🖖",
-};
-
-export const ChoiceDisplayName: Record<Choice, string> = {
-  [Choice.Rock]: "Rock",
-  [Choice.Paper]: "Paper",
-  [Choice.Scissors]: "Scissors",
-  [Choice.Lizard]: "Lizard",
-  [Choice.Spock]: "Spock",
 };
